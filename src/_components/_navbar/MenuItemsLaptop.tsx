b38fcefec0113f1bafa562dbd72c76c1ec2979da
TristanMarston <tristan@ericmarston.com>
2024-06-18 14:24:29 -0700

    key prop fix

--- src/_components/_navbar/MenuItemsLaptop.tsx
+++ src/_components/_navbar/MenuItemsLaptop.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from '@/app/context';
+import { NavLink, generateID } from '@/app/context';
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu';
 import Link from 'next/link';
 import { useEffect, useLayoutEffect, useState } from 'react';
@@ -15,7 +15,7 @@ const MenuItemDropdown = ({ title, href, dropdownOptions }: { title: string; hre
 
             <NavigationMenuContent className='grid grid-cols-2 grid-rows-2 w-[360px] items-start bg-background-light border-2 border-background-light outline-none shadow-[0_4px_30px_rgba(0,0,0,.4)]'>
                 {dropdownOptions.map(({ title, href, description }) => (
-                    <Link href={href || '#'} className='w-full flex justify-start h-full'>
+                    <Link href={href || '#'} className='w-full flex justify-start h-full' key={title + description}>
                         <NavigationMenuLink className={`hover:bg-background-lighter rounded-md flex flex-col items-left w-full h-full p-3 tracking-[-.15em] text-secondary`}>
                             <h4 className='font-semibold'>{title}</h4>
                             <p className='font-light text-[12px] tracking-tighter'>{description}</p>
@@ -66,7 +66,7 @@ const MenuItemsLaptop = ({ links }: { links: NavLink[] }) => {
     ) : (
         <div className='w-full gap-2 justify-end hidden laptop:flex mr-4'>
             {links.map(() => (
-                <Skeleton className='w-[8%] h-[20px] rounded-full' />
+                <Skeleton className='w-[8%] h-[20px] rounded-full' key={generateID(5)} />
             ))}
         </div>
     );
--- src/app/context.tsx
+++ src/app/context.tsx
@@ -5,3 +5,10 @@ export type NavLink = {
     dropdownOptions?: NavLink[];
     description?: string;
 };
+
+export const generateID = (length: number) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) result += characters.charAt(Math.floor(Math.random() * characters.length));
+    return result;
+};
