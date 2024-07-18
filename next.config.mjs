4cdaab5b07b5bab34c4b195704ab08e0e4520e13
TristanMarston <tristan@ericmarston.com>
2024-07-18 14:08:29 -0700

    remove next-purge-css

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import path from 'path';
-import withPurgeCSSModules from 'next-purge-css-modules';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-/** @type {import('next-purge-css-modules').PurgeConfig} */
-const purgeConfig = {
-    content: path.join(__dirname, 'src/**/*.{js,jsx,ts,tsx}'),
-    enableDevPurge: true,
-    fontFace: true,
-    safelist: ['body', 'html'],
-};
-
-export default withPurgeCSSModules(purgeConfig, nextConfig);
+export default nextConfig;
