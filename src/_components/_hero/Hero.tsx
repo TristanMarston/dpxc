7c550a681ddc3c0c019d01cbe3e7711d35664ec0
TristanMarston <tristan@ericmarston.com>
2024-07-18 14:11:37 -0700

    remove dynamic framer-motion loading

--- src/_components/_hero/Hero.tsx
+++ src/_components/_hero/Hero.tsx
@@ -5,9 +5,7 @@ import Link from 'next/link';
 import LinesAnimation from './LinesAnimation';
 import AutoscrollCarousel from './Carousel';
 import { delaGothic } from '@/app/context';
-import dynamic from 'next/dynamic';
-
-const MotionDiv = dynamic(() => import('framer-motion').then((mod) => mod.motion.div), { ssr: false });
+import { motion } from 'framer-motion';
 
 const Hero = () => {
     const buttons: NavLink[] = [
@@ -17,22 +15,22 @@ const Hero = () => {
 
     return (
         <>
-            <MotionDiv initial='hidden' animate='visible' transition={{ staggerChildren: 0.1 }} className='flex flex-col relative mt-28 gap-16'>
+            <motion.div initial='hidden' animate='visible' transition={{ staggerChildren: 0.1 }} className='flex flex-col relative mt-28 gap-16'>
                 <div className='flex flex-col gap-5 w-full items-center'>
                     <span className='flex justify-center w-full'>
                         <span className='w-[275px] tiny:w-[340px] mobile:w-[400px] tablet:w-[575px] taptop:w-[690px] laptop:w-[850px]'>
                             <LinesAnimation text='THIS IS' offset={0} />
                         </span>
                     </span>
-                    <MotionDiv
+                    <motion.div
                         variants={itemVariants(0.5)}
                         className={`${delaGothic.className} text-2xl text-nowrap tiny:text-[26px] min-[390px]:text-3xl mobile:text-4xl tablet:text-5xl taptop:text-6xl laptop:text-7xl text-center font-bold text-secondary tracking-wide [text-shadow:_3px_3px_3px_rgba(0,0,0)]`}
                     >
                         DOS PUEBLOS
                         <br />
                         CROSS COUNTRY
-                    </MotionDiv>
-                    <MotionDiv variants={itemVariants(0.6)} className='flex gap-4 w-full justify-center'>
+                    </motion.div>
+                    <motion.div variants={itemVariants(0.6)} className='flex gap-4 w-full justify-center'>
                         {buttons.map(({ title, href }) => (
                             <Link
                                 href={href || '#'}
@@ -42,11 +40,11 @@ const Hero = () => {
                                 {title}
                             </Link>
                         ))}
-                    </MotionDiv>
+                    </motion.div>
                 </div>
 
                 <AutoscrollCarousel />
-            </MotionDiv>
+            </motion.div>
         </>
     );
 };
